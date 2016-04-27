c8143aa83bc865fa6856e2ca3d56814134d64bb1
kangta123 <kang7213@gmail.com>
2016-04-27 17:39:45 +0800

    Update config & Add basic topic data

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var process = require('process');
 var app = express();
 var http = require('http');
+var log = require('./log');
 
 mongoose.connect('mongodb://localhost/luckstar');
 
@@ -19,7 +20,7 @@ var server = http.createServer(app);
 var port = process.env.PORT || '3000';
 
 server.listen(port);
-
+log.info('Luckystart server startup success.');
 module.exports = app;
 
 
